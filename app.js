59c43a740b52ebef2bb136af02496d56fc9a420e
nsohail <nsohail92@gmail.com>
2014-07-06 16:03:54 -0400

    added fade

---
+++ app.js
@@ -0,0 +1,40 @@
+var newLocation;
+
+$(document).ready(function(){
+
+//fade effect
+//$('body').css('display', 'none');
+
+//$('body').fadeIn(1000);
+
+
+$('.link').click(function() {
+
+event.preventDefault();
+
+linkLocation = this.href;
+
+$('body').fadeOut(1000, newpage);
+
+});
+
+function newpage() {
+
+window.location = linkLocation;
+
+}
+
+
+//icon hover effect
+$(".icon-still").mouseenter(function() {
+//debugger;
+$(this).find('.icon-action').fadeIn(300);
+});
+
+$(".icon-still").mouseleave(function() {
+$(this).find(".icon-action").fadeOut(300);
+});
+
+
+
+});
---
+++ jabargan/js/app.js
@@ -0,0 +1,28 @@
+var newLocation;
+
+$(document).ready(function(){
+
+//fade effect
+$('body').css('display', 'none');
+
+$('body').fadeIn(2000);
+
+
+$('.link').click(function() {
+
+event.preventDefault();
+
+linkLocation = this.href;
+
+$('body').fadeOut(1000, newpage);
+
+});
+
+function newpage() {
+
+window.location = linkLocation;
+
+}
+
+
+});
