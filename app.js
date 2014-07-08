07c6b22f42e4f0c2f839a413bb4db31b463f7aaa
nsohail <nsohail92@gmail.com>
2014-07-08 16:25:58 -0400

    shuffle_v1

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var newLocation;
 
 $(document).ready(function(){
 
+
+var $grid = $('#grid'),
+      $sizer = $grid.find('.shuffle__sizer');
+
+$grid.shuffle({
+    itemSelector: '.picture-item',
+    sizer: $sizer
+  });
+
+
+
+
 //fade effect
 //$('body').css('display', 'none');
 
