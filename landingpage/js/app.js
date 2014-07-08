6e1f9ab413a093307c541ab839c377cb0b84f267
nsohail <nsohail92@gmail.com>
2014-07-08 01:28:33 -0400

    Made work pages compatible

--- landingpage/js/app.js
+++ landingpage/js/app.js
@@ -25,6 +25,16 @@ window.location = linkLocation;
 }
 
 
+//icon hover effect
+$(".icon-still").mouseenter(function() {
+//debugger;
+$(this).find('.icon-action').fadeIn(300);
+});
+
+$(".icon-still").mouseleave(function() {
+$(this).find(".icon-action").fadeOut(300);
+});
+
 
 
 
